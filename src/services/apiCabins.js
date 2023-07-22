30e130929005db626786959beefc9c64b8442370
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-22 16:26:19 +0200

    feat: useMutation in deleting cabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -10,3 +10,14 @@ export const getCabins = async () => {
 
 	return data;
 };
+
+export const deleteCabin = async (id) => {
+	const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+	if (error) {
+		console.error("Cabins could not be deleted");
+		throw new Error("Cabins could not be deleted");
+	}
+
+	return data;
+};
