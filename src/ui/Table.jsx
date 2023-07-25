bd42bee3730fd0e49f5c35cec15e2be4c604f1a7
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-25 16:17:54 +0200

    fix: wrong way to pass render function in CabinTable to Table

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -85,7 +85,7 @@ const Header = ({ children }) => {
 };
 
 const Body = ({ data, render }) => {
-	if (!data.length) return <Empty>No data to show</Empty>;
+	if (!data.length) return <Empty>No data to show at the moment</Empty>;
 
 	return <StyledBody>{data.map(render)}</StyledBody>;
 };
