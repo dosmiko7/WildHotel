9ea1b3e203e6bb0e229cde7b13d1e62ca664dc95
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-25 11:42:50 +0200

    feat: extract adding cabin from Cabin to separate component

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,13 +1,9 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import Button from "../ui/Button";
 import CabinTable from "../features/cabins/CabinTable";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
-import { useState } from "react";
+import AddCabin from "../features/cabins/AddCabin";
 
 const Cabins = () => {
-	const [showForm, setShowForm] = useState(false);
-
 	return (
 		<>
 			<Row type="horizontal">
@@ -16,8 +12,7 @@ const Cabins = () => {
 			</Row>
 			<Row>
 				<CabinTable />
-				<Button onClick={() => setShowForm((show) => !show)}>Add new Cabin</Button>
-				{showForm && <CreateCabinForm />}
+				<AddCabin />
 			</Row>
 		</>
 	);
