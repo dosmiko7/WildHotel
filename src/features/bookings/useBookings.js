29962295c982b6a452a675c42241a4ffae40776e
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-27 12:38:16 +0200

    feat: added Menus button foe bookings detail

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
 import { getBookings } from "../../services/apiBookings";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export const useBookings = () => {
+	const queryClient = useQueryClient();
 	const [searchParams] = useSearchParams();
 
 	// FILTERING
@@ -25,6 +27,23 @@ export const useBookings = () => {
 		queryKey: ["bookings", filter, sortBy, page],
 		queryFn: () => getBookings({ filter, sortBy, page }),
 	});
+
+	// PRE-FATCHING
+	const pageCount = Math.ceil(count / PAGE_SIZE);
+
+	if (page < pageCount) {
+		queryClient.prefetchQuery({
+			queryKey: ["bookings", filter, sortBy, page + 1],
+			queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+		});
+	}
+
+	if (page > 1) {
+		queryClient.prefetchQuery({
+			queryKey: ["bookings", filter, sortBy, page - 1],
+			queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+		});
+	}
 
 	return { isLoading, bookings, error, count };
 };
