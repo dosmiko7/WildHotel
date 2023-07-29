bec5874dc7fb45a64ad2409a7f5cf7ae315dd929
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-29 14:01:12 +0200

    feat: created sign up form with react-hook-form

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -1,50 +1,70 @@
+import { useForm } from "react-hook-form";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
-// Email regex: /\S+@\S+\.\S+/
-
 const SignupForm = () => {
+	const { register, formState, getValues, handleSubmit } = useForm();
+	const { errors } = formState;
+
+	const onSubmit = (data) => {};
+
 	return (
-		<Form>
+		<Form onSubmit={handleSubmit()}>
 			<FormRow
 				label="Full name"
-				error={""}
+				error={errors?.fullName?.message}
 			>
 				<Input
 					type="text"
 					id="fullName"
+					{...register("fullName", { required: "This field is required" })}
 				/>
 			</FormRow>
 
 			<FormRow
 				label="Email address"
-				error={""}
+				error={errors?.email?.message}
 			>
 				<Input
 					type="email"
 					id="email"
+					{...register("email", {
+						required: "This field is required",
+						pattern: { value: /\S+@\S+\.\S+/, message: "Please provide a valid email" },
+					})}
 				/>
 			</FormRow>
 
 			<FormRow
 				label="Password (min 8 characters)"
-				error={""}
+				error={errors?.password?.message}
 			>
 				<Input
 					type="password"
 					id="password"
+					{...register("password", {
+						required: "This field is required",
+						minLength: {
+							value: 8,
+							message: "Password needs a minimum 8 characters",
+						},
+					})}
 				/>
 			</FormRow>
 
 			<FormRow
 				label="Repeat password"
-				error={""}
+				error={errors?.passwordConfirm?.message}
 			>
 				<Input
 					type="password"
 					id="passwordConfirm"
+					{...register("passwordConfirm", {
+						required: "This field is required",
+						validate: (value) => value === getValues().password || "Passwords need to match",
+					})}
 				/>
 			</FormRow>
 
