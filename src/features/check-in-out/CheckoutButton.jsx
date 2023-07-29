467afcb02752fefdbcd011eb3009451007f669cc
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-29 13:33:32 +0200

    feat: functions refactor

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,11 +1,14 @@
 import Button from "../../ui/Button";
 
-function CheckoutButton({ bookingId }) {
-  return (
-    <Button variation="primary" size="small">
-      Check out
-    </Button>
-  );
-}
+const CheckoutButton = ({ bookingId }) => {
+	return (
+		<Button
+			variation="primary"
+			size="small"
+		>
+			Check out
+		</Button>
+	);
+};
 
 export default CheckoutButton;
