3ff5c8ddd50bff53e5a963fdf8d0ebd4cab06469
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-29 17:20:20 +0200

    feat: added dark mode provider to whole app

--- src/ui/DarkModeToggle.jsx
+++ src/ui/DarkModeToggle.jsx
@@ -1,12 +1,11 @@
-import { HiOutlineMoon } from "react-icons/hi2";
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi2";
 import ButtonIcon from "./ButtonIcon";
+import { useDarkMode } from "../context/DarkModeContext";
 
 const DarkModeToggle = () => {
-	return (
-		<ButtonIcon>
-			<HiOutlineMoon />
-		</ButtonIcon>
-	);
+	const { isDarkMode, toggleDarkMode } = useDarkMode();
+
+	return <ButtonIcon onClick={toggleDarkMode}>{isDarkMode ? <HiOutlineSun /> : <HiOutlineMoon />}</ButtonIcon>;
 };
 
 export default DarkModeToggle;
