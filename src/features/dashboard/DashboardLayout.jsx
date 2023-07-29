1b981ae16dc036b4300d8c2d02f0f34510d2e3e9
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-29 18:56:40 +0200

    feat: added activity fragment in dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 import { useRecentBookings } from "./useRecentBookings";
 import { useRecentStays } from "./useRecentStays";
@@ -31,6 +32,7 @@ const DashboardLayout = () => {
 				numDays={numDays}
 				cabinCount={cabins.length}
 			/>
+			<TodayActivity />
 			<DurationChart confirmedStays={confirmedStays} />
 			<SalesChart
 				bookings={bookings}
