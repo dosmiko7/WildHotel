9bb61a018f306e87c721664631d04694f2387f4a
dosmiko7 <mikolaj.oberda@gmail.com>
2023-07-29 17:24:14 +0200

    feat: added DOM manipulation: added css classes to html element via useEffect inside DarkModeContext

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
 
 import { useLocalStorageState } from "../hooks/useLocalStorageState";
 
@@ -10,6 +10,16 @@ const DarkModeProvider = ({ children }) => {
 	const toggleDarkMode = () => {
 		setIsDarkMode((isDark) => !isDark);
 	};
+
+	useEffect(() => {
+		if (isDarkMode) {
+			document.documentElement.classList.add("dark-mode");
+			document.documentElement.classList.remove("light-mode");
+		} else {
+			document.documentElement.classList.add("light-mode");
+			document.documentElement.classList.remove("dark-mode");
+		}
+	}, [isDarkMode]);
 
 	return <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>{children}</DarkModeContext.Provider>;
 };
